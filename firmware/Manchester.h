d09d246271d7aa8f42b8a4412f2b4e8a1e89010f
Paul Kourany <pkourany@users.noreply.github.com>
2014-10-01 11:20:30 -0400

    Update Manchester.h

--- firmware/Manchester.h
+++ firmware/Manchester.h
@@ -34,7 +34,7 @@ The data rate is then 600 bits/s. Higher and lower rates are also supported.
 #define SPARK	1		//Temporary until done by IDE compiler
 
 #include "application.h"
-#include "SparkIntervalTimer/SparkIntervalTimer.h"
+#include "../SparkIntervalTimer/SparkIntervalTimer.h"
 
 
 //timer scaling factors for different transmission speeds
